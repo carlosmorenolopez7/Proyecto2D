84bef67c6e4f8d8d6c584855f66151b0f8f7465e
Carlos Moreno López <116076184+carlosmorenolopez7@users.noreply.github.com>
2024-11-20 12:40:31 +0100

    menu de pausa

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -2,22 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public void SetVolume(float volume)
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
     {
-        audioMixer.SetFloat("volume", volume);
+        if (!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1f);
+        }
+
+        if (!PlayerPrefs.HasKey("fullscreen"))
+        {
+            PlayerPrefs.SetInt("fullscreen", Screen.fullScreen ? 1 : 0);
+        }
+
+        if (!PlayerPrefs.HasKey("quality"))
+        {
+            PlayerPrefs.SetInt("quality", QualitySettings.GetQualityLevel());
+        }
+
+        Load();
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
     }
 
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        Save();
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+        Save();
+    }
+
+    private void Load() 
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+        Screen.fullScreen = PlayerPrefs.GetInt("fullscreen") == 1;
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("quality"));
+    }
+
+    private void Save() 
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+        PlayerPrefs.SetInt("fullscreen", Screen.fullScreen ? 1 : 0);
+        PlayerPrefs.SetInt("quality", QualitySettings.GetQualityLevel());
     }
 }
